c711bd8b98857c8d4e64cda6374512c709f9787b
REALSTEVEIG <101066723+REALSTEVEIG@users.noreply.github.com>
2022-12-31 20:39:38 +0100

    username

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,7 +1,8 @@
 
 exports.dashboard = (req, res) => {
     try {
-        res.render('dashboard', {title : "DASHBOARD"})
+        const user = req.user.username
+        res.render('dashboard', {title : "DASHBOARD", user})
     } catch (error) {
         console.log(error)
         return res.redirect('login')
